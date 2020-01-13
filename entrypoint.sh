491e091ae3845a3ece291d67220f5127a8d77e86
jiro4989 <jiroron666@gmail.com>
2020-01-13 18:18:54 +0900

    fix entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,20 +6,24 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 nim --version
 
-nim check $INPUT_SRC 2>&1 > /dev/null |
-  grep -E "^([^)]+)\) (Hint|Warning|Error): .*\[[^]]+\]$" |
-  sed \
-    -e "s/(/:/" \
-    -e "s/, /:/" \
-    -e "s/) /:/"
+for f in $INPUT_SRC; do
+  nim check "$f" 2>&1 > /dev/null |
+    grep -E "^([^)]+)\) (Hint|Warning|Error): .*\[[^]]+\]$" |
+    sed \
+      -e "s/(/:/" \
+      -e "s/, /:/" \
+      -e "s/) /:/"
+done
 
-nim check $INPUT_SRC 2>&1 > /dev/null |
-  grep -E "^([^)]+)\) (Hint|Warning|Error): .*\[[^]]+\]$" |
-  sed \
-    -e "s/(/:/" \
-    -e "s/, /:/" \
-    -e "s/) /:/" |
-  reviewdog -efm="%f:%l:%c:%m" -name="nimlint" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
+for f in $INPUT_SRC; do
+  nim check "$f" 2>&1 > /dev/null |
+    grep -E "^([^)]+)\) (Hint|Warning|Error): .*\[[^]]+\]$" |
+    sed \
+      -e "s/(/:/" \
+      -e "s/, /:/" \
+      -e "s/) /:/" |
+    reviewdog -efm="%f:%l:%c:%m" -name="nimlint" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
+done
 
 # if [ "${INPUT_REPORTER}" == 'github-pr-review' ]; then
 #   # Use github-pr-review reporter to format result to include link to rule page.
