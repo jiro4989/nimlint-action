46f343a13d077e9e6c4fbe7a7fda2649ede9c0ce
jiro4989 <jiroron666@gmail.com>
2020-01-13 17:36:44 +0900

    wi

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ nim check $INPUT_SRC 2>&1 > /dev/null |
 
 nim check $INPUT_SRC 2>&1 > /dev/null |
   grep -E "^([^)]+)\) (Hint|Warning|Error): .*\[[^]]+\]$" |
-  reviewdog -efm="%f(%l, %c) %m" -name="nimlint" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
+  reviewdog -efm="%f(%l, %c) %m" -name="nimlint" -reporter="${INPUT_REPORTER:-github-pr-check}"
 
 # if [ "${INPUT_REPORTER}" == 'github-pr-review' ]; then
 #   # Use github-pr-review reporter to format result to include link to rule page.
