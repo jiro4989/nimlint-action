de8d382669e70466081d2a36fa1486e1b6409a72
jiro4989 <jiroron666@gmail.com>
2020-01-13 18:34:25 +0900

    remove code

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,14 +13,7 @@ for f in $INPUT_SRC; do
       -e "s/(/:/" \
       -e "s/, /:/" \
       -e "s/) /:/"
-done
+done |
+  tee check.log
 
-for f in $INPUT_SRC; do
-  nim check "$f" 2>&1 > /dev/null |
-    grep -E "^([^)]+)\) (Hint|Warning|Error): .*" |
-    sed \
-      -e "s/(/:/" \
-      -e "s/, /:/" \
-      -e "s/) /:/" |
-    reviewdog -efm="%f:%l:%c:%m" -name="nimlint" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
-done
+cat check.log | reviewdog -efm="%f:%l:%c:%m" -name="nimlint" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
